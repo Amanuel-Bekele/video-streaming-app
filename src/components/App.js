dfe2cb71615043a1fbeb3c02678b98a411120e32
Ammanuel Bekele <ambek@Ammanuels-MacBook-Pro.local>
2019-07-16 00:38:39 -0500

    Added Sematic UI to dependencies, Created Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import StreamCreate from '../components/streams/StreamCreate'
 import StreamEdit from '../components/streams/StreamEdit'
 import StreamDelete from '../components/streams/StreamDelete'
 import StreamShow from '../components/streams/StreamShow'
 import StreamList from '../components/streams/StreamList'
+import Header from './Header'
 
 
 const App = () => {
     return (
-        <div>
+        <div className="ui container">
             <BrowserRouter>
                 <div>
+                <Header />
                     <Route path="/" exact component={StreamList} />
                     <Route path="/streams/new" exact component={StreamCreate} />
                     <Route path="/streams/edit" exact component={StreamEdit} />
