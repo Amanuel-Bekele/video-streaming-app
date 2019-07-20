85a2afa8e9682e6c16a6d440c10906f776fe8316
Ammanuel Bekele <ambek@Ammanuels-MacBook-Pro.local>
2019-07-20 16:56:57 -0500

    Change BrowserRouter to Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 
 import StreamCreate from '../components/streams/StreamCreate'
 import StreamEdit from '../components/streams/StreamEdit'
@@ -7,21 +7,23 @@ import StreamDelete from '../components/streams/StreamDelete'
 import StreamShow from '../components/streams/StreamShow'
 import StreamList from '../components/streams/StreamList'
 import Header from './Header'
+import history from '../history'
+
 
 
 const App = () => {
     return (
         <div className="ui container">
-            <BrowserRouter>
+            <Router history={history}>
                 <div>
-                <Header />
+                    <Header />
                     <Route path="/" exact component={StreamList} />
                     <Route path="/streams/new" exact component={StreamCreate} />
                     <Route path="/streams/edit" exact component={StreamEdit} />
                     <Route path="/streams/delete" exact component={StreamDelete} />
                     <Route path="/streams/show" exact component={StreamShow} />
                 </div>
-            </BrowserRouter>
+            </Router>
         </div>
     )
 }
